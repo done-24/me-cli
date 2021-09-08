d07bd56336e7e84ebd975cdb595eb34304c3d801
done-24 <summer_fi2021@163.com>
2021-09-08 23:52:00 +0800

    feat: prompts test

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,37 @@
 #! /usr/bin/env node
 
-console.log('init')
+const path = require('path')
+const fs = require('fs-extra')
+const argv = require('minimist')(process.argv.slice(2), {string: ['_']})
+const prompts = require('prompts')
+const { yellow, red } = require('kolorist')
+
+const cwd = process.cwd()
+
+async function init() {
+  let result = {}
+
+  try {
+    result = await prompts([
+      {
+        type: 'text',
+        name: 'meaning',
+        message: 'What is the meaning of life?'
+      },
+      {
+        onCancel: () => {
+          throw new Error(red('✖') + ' Operation cancelled')
+        }
+      }
+    ])
+  } catch (cancelled) {
+    console.log(cancelled.message)
+    return
+  }
+
+  console.log(result)
+}
+
+init().catch(e => {
+  console.error(e)
+})
